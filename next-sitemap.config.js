48461e53fd9fe8ed4061fda42cd9fbca124e4fdc
ferasraj <feras.raj@gmail.com>
2025-07-10 05:59:56 +0300

    \sitemap-0.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,15 @@
-// const siteMetadata = require("./src/utils/siteMetaData");
+const siteMetadata = require("./src/utils/siteMetaData");
 
-// module.exports = {
-//   siteUrl: siteMetadata.siteUrl,
-//   generateRobotsTxt: true,
-// };
-
-import siteMetadata from "./src/utils/siteMetaData";
-
-const config = {
+module.exports = {
   siteUrl: siteMetadata.siteUrl,
   generateRobotsTxt: true,
 };
 
-export default config;
+// import siteMetadata from "./src/utils/siteMetaData";
+
+// const config = {
+//   siteUrl: siteMetadata.siteUrl,
+//   generateRobotsTxt: true,
+// };
+
+// export default config;
