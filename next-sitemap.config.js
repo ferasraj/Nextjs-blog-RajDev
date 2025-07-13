e262e536036a0303bb19659dde789f8e5a70af7a
ferasraj <feras.raj@gmail.com>
2025-07-13 04:57:39 +0300

    change robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,12 +4,3 @@ module.exports = {
   siteUrl: siteMetadata.siteUrl,
   generateRobotsTxt: true,
 };
-
-// import siteMetadata from "./src/utils/siteMetaData";
-
-// const config = {
-//   siteUrl: siteMetadata.siteUrl,
-//   generateRobotsTxt: true,
-// };
-
-// export default config;
